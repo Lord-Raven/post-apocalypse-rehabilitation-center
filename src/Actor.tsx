0c6e1f94c86f0150b599e4d536ae32f2c535c126
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-11-06 03:08:10 +0000

    Try to build actors.

--- src/Actor.tsx
+++ src/Actor.tsx
@@ -3,7 +3,9 @@ import { EmotionPack } from "./Emotion";
 class Actor {
     id: string;
     name: string;
+    avatarImageUrl: string;
     physicalDescription: string;
+    personalityDescription: string;
     emotionPack: EmotionPack;
 
     // Characters are candidates for a rehabilitation program; the are coming into the program from a vast range of past life situations.
@@ -11,21 +13,23 @@ class Actor {
     // They will be prepped for completely new lives in a sci-fi, dystopian future setting where they may be valued for different traits.
     // Graded stats from 1-10; these get translated to a letter grade in the UI
     capability: number; // Skill grade 1-10
-    health: number; // Health grade 1-10
+    condition: number; // Condition grade 1-10
     resilience: number; // Resilience grade 1-10
-    sociability: number; // Sociability grade 1-10
+    personality: number; // Personality grade 1-10
     sexuality: number; // Sexuality grade 1-10
     compliance: number; // Compliance grade 1-10
 
-    constructor(id: string, name: string, physicalDescription: string, emotionPack: EmotionPack, capability: number, health: number, resilience: number, sociability: number, sexuality: number, compliance: number) {
+    constructor(id: string, name: string, avatarImageUrl: string, physicalDescription: string, personalityDescription: string, emotionPack: EmotionPack, capability: number, condition: number, resilience: number, personality: number, sexuality: number, compliance: number) {
         this.id = id;
         this.name = name;
+        this.avatarImageUrl = avatarImageUrl;
         this.physicalDescription = physicalDescription;
+        this.personalityDescription = personalityDescription;
         this.emotionPack = emotionPack;
         this.capability = capability;
-        this.health = health;
+        this.condition = condition;
         this.resilience = resilience;
-        this.sociability = sociability;
+        this.personality = personality;
         this.sexuality = sexuality;
         this.compliance = compliance;
     }
