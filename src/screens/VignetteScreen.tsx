8b118c4db96d636c1c4e40e4867c1a89cca25a39
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-11-16 04:08:35 +0000

    Fixing background.

--- src/screens/VignetteScreen.tsx
+++ src/screens/VignetteScreen.tsx
@@ -426,13 +426,11 @@ export const VignetteScreen: FC<VignetteScreenProps> = ({ stage, setScreenType }
         setCharacterStatChanges(changes);
     };
 
-    console.log(`Rendering VignetteScreen in module ${vignette.moduleId}:`, stage().getSave().layout.getModuleById(vignette.moduleId || '')?.getAttribute('defaultImageUrl') );
-
     return (
         <BlurredBackground
             imageUrl={stage().getSave().layout.getModuleById(vignette.moduleId || '')?.getAttribute('defaultImageUrl') || ''}
         >
-            <div style={{ position: 'relative', width: '100vw', height: '100vh', background: '#000' }}>
+            <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
 
             {/* Actors */}
             <div style={{ position: 'absolute', inset: 0, zIndex: 1, pointerEvents: 'none' }}>
