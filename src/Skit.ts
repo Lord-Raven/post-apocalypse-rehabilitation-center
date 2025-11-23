0fe1dc05e7b1da2a8262901404ba14b93a01ff6d
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-11-23 18:35:19 +0000

    Skit generation issues.

--- src/Skit.ts
+++ src/Skit.ts
@@ -102,10 +102,10 @@ export async function generateSkitScript(skit: SkitData, stage: Stage): Promise<
         `The PARC is an isolated station near a black hole. It serves as both sanctuary and containment for its diverse inhabitants, who hail from various alternate realities. ` +
         `${playerName} is the only non-patient aboard the station (although they may hire patients on as crew or staff); as a result, the station may feel a bit lonely or alienating at times. ` +
         `Much of the day-to-day maintenance and operation of the station is automated by the station's AI systems and various drones, enabling ${playerName} to focus on patient care and rehabilitation.` +
-        (stage.getSave().stationStats ? (
+        /*(stage.getSave().stationStats ? (
             `\n\nThe PARC's current stats and impacts:\n` +
             Object.entries(stage.getSave().stationStats || {}).map(([stat, value]) => `  ${stat.toUpperCase()} (${value}): ${STATION_STAT_PROMPTS[stat as StationStat][getStatRating(value)]}`).join('\n')
-        ) : '') +
+        ) : '') +*/
         `\n\n${playerName}'s profile: ${stage.getSave().player.description}` +
         // List characters who are here, along with full stat details:
         `\n\nPresent Characters:\n${presentActors.map(actor => {
@@ -128,9 +128,9 @@ export async function generateSkitScript(skit: SkitData, stage: Stage): Promise<
         `\n\nStats:\n${Object.values(Stat).map(stat => `${stat.toUpperCase()}: ${getStatDescription(stat)}`).join('\n')}` +
         `\n\nEmotions:\n${Object.values(Emotion).map(emotion => `${emotion.toUpperCase()}`).join(', ')}` +
         `\n\nScene Prompt:\n${generateSkitPrompt(skit, stage, skit.script.length > 0)}` +
-        /*(module ? (`\n\nModule Details:\n  This scene is set in ` +
+        (module ? (`\n\nModule Details:\n  This scene is set in ` +
             `${module.type === 'quarters' ? `${moduleOwner ? `${moduleOwner.name}'s` : 'a vacant'} quarters` : 
-            `the ${module.type || 'Unknown'}`}. ${module.getAttribute('skitPrompt') || 'No description available.'}\n`) : '') +*/
+            `the ${module.type || 'Unknown'}`}. ${module.getAttribute('skitPrompt') || 'No description available.'}\n`) : '') +
         `\n\nExample Script Format:\n` +
         'System: CHARACTER NAME: They do actions in prose. "Their dialogue is in quotation marks."\nANOTHER CHARACTER NAME: [ANOTHER CHARACTER EXPRESSES JOY][CHARACTER NAME EXPRESSES SURPRISE] "Dialogue in quotation marks."\nNARRATOR: [CHARACTER NAME EXPRESSES RELIEF] Descriptive content that is not attributed to a character.' +
         `\n\nExample Ending Script Format:\n` +
