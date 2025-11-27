6a75d6ca0c3ca86e4d25c1a3695a89d9604d7632
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-11-27 02:07:37 +0000

    Request fulfillment.

--- src/screens/StationScreen.tsx
+++ src/screens/StationScreen.tsx
@@ -1051,7 +1051,7 @@ export const StationScreen: FC<StationScreenProps> = ({stage, setScreenType}) =>
                                                     onClick={() => {
                                                         console.log(`Clicked request ${request.id}`);
                                                     }}
-                                                    onFulfill={() => request.fulfill(stage())}
+                                                    onFulfill={(actorId) => request.fulfill(stage(), actorId)}
                                                 />
                                             ));
                                         })()}
